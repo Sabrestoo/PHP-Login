594676bec69a2a02fba6ed329b8c0d25acfa34b1
Stuart Summers <summers2483@hotmail.com>
2020-08-07 12:06:41 +0200

    Login and register pages added

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('__CONFIG__')) {
+    //Redirect or 404 page here instead of exit
+    exit('You do not have a config file');
+}
+
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+
+define('__CONFIG__', true);
+
+require_once './includes/config.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,35 +14,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="robots" content="follow">
     <title>Page Title</title>
-    <base href="/" />
+    <base href="" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
   </head>
   <body>
-  	<!-- <div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
-			</form>
-  		</div>
-  	</div> -->
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
-  </body>
-</html>
+  	<div class="uk-section uk-container">
+	  <?php
+		  
+		  echo "Hello World \r\n" . date("d-m-Y");
+	
+		  ?>
+
+		  <a href="/php-login/login.php">Login</a>
+		  <a href="/php-login/register.php">Register</a>
+	  </div>
+	  
+	  <?php require_once './includes/footer.php'; ?>
