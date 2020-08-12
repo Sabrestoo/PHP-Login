d98e49506351979b33ac9407e062a20cca9b74c3
Stuart Summers <summers2483@hotmail.com>
2020-08-12 12:07:21 +0200

    Amended assets

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).on('submit', 'form.js-register', function(event) {
+    event.preventDefault();
+
+       alert('form was submitted');
+        
+    return false;
+});
--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
     <!-- UIkit JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
-    <script src="./php-login/assets/main.js"></script>
+    <script src="./php-login/assets/js/main.js"></script>
   </body>
 </html>
